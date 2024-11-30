f5e6e5d17ac0f8b03100ae7918f0f0087641f0d7
qiuliwei <1807191473@qq.com>
2024-11-30 21:52:13 +0800

    2024/11/30 2

--- demo16_vue3_anle/src/App.vue
+++ demo16_vue3_anle/src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <!-- 主题提供 :theme="darkTheme" -->
-  <n-config-provider :theme="darkTheme" >
-      <RouterView/>
-  </n-config-provider>
+
+  <RouterView/>
+
 </template>
 
 <script setup>
-import { darkTheme } from 'naive-ui';
 </script>
 
 <style scoped>
