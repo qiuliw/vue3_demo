cd7b27818e0742fadf29ff34a471646a5c5fad2b
qiuliw <1807191473@qq.com>
2024-12-19 16:15:48 +0800

    echarts图表

---
+++ demo16_vue3_anle/src/components/QEcharts.vue
@@ -0,0 +1,59 @@
+<template>
+    <div ref="chart" style="width: 600px; height: 400px;"></div>
+  </template>
+  <script>
+  import * as echarts from 'echarts';
+  export default {
+    name: 'Chart',
+    mounted() {
+      this.initChart();
+    },
+    methods: {
+      initChart() {
+        const chart = echarts.init(this.$refs.chart);
+        const option = {
+          title: {
+            text: '上香天数榜',
+            textStyle: {
+              color: '#fff' // 设置标题文字颜色为白色
+            }
+          },
+          tooltip: {
+            textStyle: {
+              color: '#fff' // 设置提示框文字颜色为白色
+            }
+          },
+          xAxis: {
+            axisLabel: {
+              textStyle: {
+                color: '#fff' // 设置x轴标签文字颜色为白色
+              }
+            },
+            data: ['其妹-张燕', '其子-孙散', '其女-孙离', '其孙-孙损', '亲戚-李丽']
+          },
+          yAxis: {
+            axisLabel: {
+              textStyle: {
+                color: '#fff' // 设置y轴标签文字颜色为白色
+              }
+            }
+          },
+          series: [{
+            name: '天',
+            type: 'bar',
+            data: [743, 323, 121, 68, 33],
+            label: {
+              show: true,
+              textStyle: {
+                color: '#fff' // 设置柱状图数据标签文字颜色为白色
+              }
+            }
+          }]
+        };
+        chart.setOption(option);
+      }
+    }
+  };
+  </script>
+  <style scoped>
+  </style>
--- demo16_vue3_anle/src/views/Page4.vue
+++ demo16_vue3_anle/src/views/Page4.vue
@@ -1,24 +1,38 @@
 <template>
-  <div style="display: flex;flex-direction: column;">
-    <div style="display: flex;justify-content: center;">
-      <n-gradient-text :size="50" type="success">
-        今日香王
-      </n-gradient-text>
+  <div style="display: flex;width: 100vw;height: 100vh;margin:100px;">
+    <!-- 人物 -->
+    <div style="flex:1;display: flex;flex-direction: column;justify-content: center;align-content: center;">
+      <div style="display: flex;justify-content: center;align-items: center;">
+        <span>
+          <n-gradient-text :size="50" type="success">
+            今日香王
+          </n-gradient-text>
+        </span>
+      </div>
+      <div style="display: flex;justify-content: center;">
+        <n-card @Click="handleClick" title="李芳" :bordered="false" hoverable>
+          <template #cover>
+            <img class="img" src="../../public/imgs/a-1.jpeg">
+          </template>
+          李芳，慈爱母亲，以家庭为荣，温暖如春日阳光。2024年安详离世，享年76岁，爱永留心间。
+          <n-statistic style="margin-top: 20px;" label="今日有" tabular-nums>
+            <n-number-animation ref="numberAnimationInstRef" :from="0" :to="12039" />
+            <template #suffix>
+              位后代为其上香
+            </template>
+          </n-statistic>
+        </n-card>
+      </div>
 
     </div>
-    <n-card @Click="handleClick"  title="李芳" :bordered="false" hoverable>
-      <template #cover>
-        <img class="img" src="../../public/imgs/a-1.jpeg">
-      </template>
-      李芳，慈爱母亲，以家庭为荣，温暖如春日阳光。2024年安详离世，享年76岁，爱永留心间。
-      <n-statistic style="margin-top: 20px;" label="今日有" tabular-nums>
-        <n-number-animation ref="numberAnimationInstRef" :from="0" :to="12039" />
-        <template #suffix>
-          位后代为其上香
-        </template>
-      </n-statistic>
-    </n-card>
+    <!-- 数据 -->
+    <div class="datashow" style="">
+      <NSpace>
+        <QEcharts />
+      </NSpace>
+    </div>
   </div>
+
 
 </template>
 
@@ -27,9 +41,25 @@
   max-width: 300px;
   max-height: 500px;
 }
+
+.datashow {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (max-width:800px) {
+  .datashow {
+    display: none;
+  }
+}
 </style>
 
 <script setup>
+import QEcharts from '@/components/QEcharts.vue';
+import { NCard, NSpace } from 'naive-ui';
+
 
 const numberAnimationInstRef = ref(null);
 function handleClick() {
